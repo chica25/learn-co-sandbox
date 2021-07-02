fddacfa3956a145421ba91aac281b482e7a6a2ba
Patricia Cordero <pfco520@gmail.com>
2021-07-02 01:07:29 +0000

    Automatically backed up by Learn

---
+++ practice.rb
@@ -0,0 +1,17 @@
+
+class Student < ActiveRecord::Base
+end
+
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/students.sqlite"
+)
+
+
+sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS students (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+  )
+SQL
